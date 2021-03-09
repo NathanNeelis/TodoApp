c12054241e4daa30cdf3f8f805993d540401d599
NathanNeelis <nathan.neelis@hva.nl>
2021-03-09 20:26:48 +0100

    first break code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log('hello world')
+
+// toevoeg knop
+const addButton = document.getElementById('add-button');
+const toDoEntryBox = document.getElementById('todo-entry-box');
+const toDoList = document.getElementById('todo-list');
+addButton.addEventListener('click', addToDoItem);
+
+function addToDoItem() {
+    const entryText = toDoEntryBox.value
+    newToDoItem(entryText);
+}
+
+
+// nieuwe todo's toevoegen
+
+function newToDoItem(entryText) {
+    let toDoItem = document.createElement("li");
+    let toDoText = document.createTextNode(entryText);
+
+    toDoItem.appendChild(toDoText);
+    toDoList.appendChild(toDoItem);
+}
